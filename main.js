7454210b0c33c7fb8948de469045798cc66ee830
matthewinman0 <matthewinman321@gmail.com>
2026-05-16 23:23:52 +0100

    Changed all maptiler apis to opensource

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
 let map;
 let is3D = false;
 
+var demSource = new mlcontour.DemSource({
+  url: "https://s3.amazonaws.com/elevation-tiles-prod/terrarium/{z}/{x}/{y}.png",
+  encoding: "terrarium", // "mapbox" or "terrarium" default="terrarium"
+  maxzoom: 13,
+  worker: true, // offload isoline computation to a web worker to reduce jank
+  cacheSize: 100, // number of most-recent tiles to cache
+  timeoutMs: 10_000, // timeout on fetch requests
+});
+demSource.setupMaplibre(maplibregl);
+
+
 //  Style Loader - fetches and merges style layers
 async function loadStyle() {
   const base = await (await fetch("./style/base.json")).json();
@@ -78,7 +89,7 @@ window.mapReady = loadStyle().then(style => {
     style,
     center: [-2.5420, 54.0022],
     zoom: 5
-  });
+});
 
   window.map = map;
 
@@ -91,10 +102,64 @@ window.mapReady = loadStyle().then(style => {
     );
     map.addControl(
         new maplibregl.TerrainControl({
-            source: 'terrain',
+            source: '3d terrain',
             exaggeration: parseFloat(document.getElementById("terrain-exaggeration").value)
         })
     );
+    
+    map.addSource("contour-source", {
+      type: "vector",
+      tiles: [
+        demSource.contourProtocolUrl({
+          // convert meters to feet, default=1 for meters
+          multiplier: 3.28084,
+          thresholds: {
+            // zoom: [minor, major]
+            11: [200, 1000],
+            12: [100, 500],
+            14: [50, 200],
+            15: [20, 100],
+          },
+          // optional, override vector tile parameters:
+          contourLayer: "contours",
+          elevationKey: "ele",
+          levelKey: "level",
+          extent: 4096,
+          buffer: 1,
+        }),
+      ],
+      maxzoom: 15,
+    });
+
+    map.addLayer({
+      id: "contour-lines",
+      type: "line",
+      source: "contour-source",
+      "source-layer": "contours",
+      paint: {
+        "line-color": "rgba(0,0,0, 50%)",
+        // level = highest index in thresholds array the elevation is a multiple of
+        "line-width": ["match", ["get", "level"], 1, 1, 0.5],
+      },
+    });
+    map.addLayer({
+      id: "contour-labels",
+      type: "symbol",
+      source: "contour-source",
+      "source-layer": "contours",
+      filter: [">", ["get", "level"], 0],
+      layout: {
+        "symbol-placement": "line",
+        "text-size": 10,
+        "text-field": ["concat", ["number-format", ["get", "ele"], {}], "'"],
+        "text-font": ["Noto Sans Bold"],
+      },
+      paint: {
+        "text-halo-color": "white",
+        "text-halo-width": 1,
+      },
+    });
+
     map.setTerrain(null);
     toggleBuildings();
 
@@ -137,14 +202,13 @@ document.getElementById("export-settings-btn").onclick = () => {
   document.getElementById("export-settings").style.display = "block";
 };
 
-
 // Terrain exaggeration control (broken)
 document.getElementById("terrain-exaggeration").onchange = (e) => {
   const value = parseFloat(e.target.value);
   const terrain = map.getTerrain();
 
   map.setTerrain({
-    source: "terrain",
+    source: "3d terrain",
     exaggeration: value
   });
 };
